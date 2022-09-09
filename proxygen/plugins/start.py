f42783edf6cbc4679d6f397c70f618e8b7b3bbfa
EfeApakDev <93475651+EfeApakDev@users.noreply.github.com>
2022-09-09 03:20:06 +0300

    Update start.py

--- proxygen/plugins/start.py
+++ proxygen/plugins/start.py
@@ -6,33 +6,31 @@ from Configs import Config
 btn =[
   [Button.inline("Https", data="https")],
   [Button.inline("Socks4", data="socks4"), Button.inline("Socks5", data="socks5")],
-  [Button.url("My Source Code", "https://github.com/TgxBotz/ProxyGenBot")]
+  [Button.url("Sahibim ⚡", "https://t.me/sancakbegi")]
   ]
 
 @ProxyGen.on(events.NewMessage(pattern="^[!?/]start$"))
 async def start(event):
 
     Text = """
-**Heya {}**
-I am free proxygenbot which
-can generate any type of proxies
-for you!
-__Click on the button below!__
+**Selam🇹🇷 {}**
+BEN ÜCRETSİZ PROXY ÜRETEN BİR BOTUM
+HER TÜRLÜ PROXY ÜRETEBİLİRİM
+__Aşağıdaki butona tıklayın!__
 """.format(event.sender.first_name)
 
     if event.is_group and event.chat_id != Config.LOGS_CHAT:
-      await event.reply("**I dont work in groups!**\n__I am leaving this chat!__")
+      await event.reply("** gruplarda çalışmıyorum!**\n__görüşürüz sohbetten ayrılıyorum!__")
       await ProxyGen.delete_dialog(event.chat_id)
       return
 
-    await ProxyGen.send_message(Config.LOGS_CHAT, f"Bot started by [{event.sender.first_name}](tg://user?id={event.sender_id}).")
-    await event.reply(Text, buttons=[[Button.inline("Generate Proxies", data="proxy")]])
+    await ProxyGen.send_message(Config.LOGS_CHAT, f"botu başlatan [{event.sender.first_name}](tg://user?id={event.sender_id}).")
+    await event.reply(Text, buttons=[[Button.inline("Proxy oluştur ", data="proxy")]])
 
 @ProxyGen.on(events.CallbackQuery(data="proxy"))
 async def k(event):
     Text = """
-**Heya {}
-Choose the proxies you
-want to generate:**
+**Merhaba {}
+Oluşturmak istediğiniz proxy'leri seçin:**
 """.format(event.sender.first_name)
     await event.edit(Text, buttons=btn)
